8453e3c34de32b4f7a073f9a50f95c881e78a331
Vindhya28 <vindhya28@gmail.com>
2016-10-03 22:14:49 -0400

    commit test2

--- src/test/java/com/mongodb/AppTest.java
+++ src/test/java/com/mongodb/AppTest.java
@@ -4,6 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import junit.framework.Test.*;
+import junit.framework.1.*
 
 /**
  * Unit test for simple App.
